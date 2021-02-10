c83fde1fb7899fcf16819d8d07390e35bc4f88a1
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-02-10 13:20:00 +0200

    some changes in the code better description

--- Program.cs
+++ Program.cs
@@ -7,25 +7,23 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            int[] numbers = {5, 6, 10, 50, 70, -1, 0 };
-            // return the index with the min elements/
-            //int minNumber = numbers.Min();
-            int minNumberIndex = 0;
+            int[] numbers = { 5, 6, 10, 50, 70, -1, 0 };
+            // return the index of the min element/            
+            int minNumberIndex = 0; //we acceptrst number is the min with his indexNr/
             int minNumb = numbers[0];
 
             for (int i = 0; i < numbers.Length; i++)
             {
                 int currNum = numbers[i];
-                // int minNumb = currNum; //We accept that our first array number is the min
 
-                for (int j = i+1; j < numbers.Length; j++)
+                for (int j = i + 1; j < numbers.Length; j++)
                 {
                     if (currNum > numbers[j])
                     {
                         minNumb = numbers[i + 1];
                         minNumberIndex = i + 1;
                     }
-                } 
+                }
             }
 
             Console.WriteLine($"minIndex is: {minNumberIndex}");
